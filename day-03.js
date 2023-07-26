a3897f3c5ceb80c4b20c3d46c7154fb60f592caf
ivanchornyi-co <ivanchornyi.co@gmail.com>
2023-07-26 19:31:57 +0300

    fix all files

--- day-03.js
+++ day-03.js
@@ -1,5 +1,41 @@
 //http://www.codewars.com/kata/571f832f07363d295d001ba8
 
+function trueOrFalse(val) {
+  if (val == false || val == null) return 'false';
+  else return 'true';
+}
+
 //http://www.codewars.com/kata/57202aefe8d6c514300001fd
 
+function saleHotdogs(n) {
+  if (n < 5) return n * 100;
+  else if (n >= 5 && n < 10) return n * 95;
+  else return n * 90;
+}
+
 //http://www.codewars.com/kata/572059afc2f4612825000d8a
+
+function howManydays(month) {
+  let days;
+  switch (month) {
+    case 2:
+      days = 28;
+      break;
+    case 4:
+      days = 30;
+      break;
+    case 6:
+      days = 30;
+      break;
+    case 9:
+      days = 30;
+      break;
+    case 11:
+      days = 30;
+      break;
+
+    default:
+      days = 31;
+  }
+  return days;
+}
---
+++ day-04.js
@@ -0,0 +1,7 @@
+//http://www.codewars.com/kata/57216d4bcdd71175d6000560
+
+//http://www.codewars.com/kata/5721a78c283129e416000999
+
+//http://www.codewars.com/kata/5721c189cdd71194c1000b9b
+
+//http://www.codewars.com/kata/5722b3f0bd5583cf44001000
