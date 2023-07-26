89f48f13eff8cc2980ebde59761daf27b863eaea
ivanchornyi-co <ivanchornyi.co@gmail.com>
2023-07-26 09:37:56 +0300

    task 1 end

---
+++ day-02.js
@@ -0,0 +1,44 @@
+//http://www.codewars.com/kata/571edd157e8954bab500032d
+
+let v1 = 50,
+  v2 = 100,
+  v3 = 150,
+  v4 = 200,
+  v5 = 2,
+  v6 = 250;
+
+function equal1() {
+  let a = v1,
+    b = v1;
+  return a + b;
+}
+
+function equal2() {
+  let a = v4,
+    b = v2;
+  return a - b;
+}
+
+function equal3() {
+  let a = v5,
+    b = v1;
+  return a * b;
+}
+
+function equal4() {
+  let a = v4,
+    b = v5;
+  return a / b;
+}
+
+function equal5() {
+  let a = v6,
+    b = v3;
+  return a % b;
+}
+
+//http://www.codewars.com/kata/571edea4b625edcb51000d8e
+
+//http://www.codewars.com/kata/571effabb625ed9b0600107a
+
+//http://www.codewars.com/kata/571f1eb77e8954a812000837
