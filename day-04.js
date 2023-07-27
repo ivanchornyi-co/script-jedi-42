f73c8a6cf707206d285dad3754758beae35ee2bc
ivanchornyi-co <ivanchornyi.co@gmail.com>
2023-07-27 11:37:16 +0300

    task 1 done

--- day-04.js
+++ day-04.js
@@ -1,5 +1,19 @@
 //http://www.codewars.com/kata/57216d4bcdd71175d6000560
 
+function padIt(str, n) {
+  let i = 0;
+  let result = str;
+  while (i < n) {
+    if (i % 2 === 0) {
+      result = '*' + result;
+    } else {
+      result = result + '*';
+    }
+    i++;
+  }
+  return result;
+}
+
 //http://www.codewars.com/kata/5721a78c283129e416000999
 
 //http://www.codewars.com/kata/5721c189cdd71194c1000b9b
