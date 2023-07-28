4c74f84a46ab33254c665513cd368c8a54720fe3
ivanchornyi-co <ivanchornyi.co@gmail.com>
2023-07-28 22:40:00 +0300

    day-04 end

--- day-04.js
+++ day-04.js
@@ -16,6 +16,49 @@ function padIt(str, n) {
 
 //http://www.codewars.com/kata/5721a78c283129e416000999
 
+function pickIt(arr) {
+  let odd = [];
+  let even = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      even.push(arr[i]);
+    } else {
+      odd.push(arr[i]);
+    }
+  }
+
+  return [odd, even];
+}
+
 //http://www.codewars.com/kata/5721c189cdd71194c1000b9b
 
+function grabDoll(dolls) {
+  let bag = [];
+  for (let i = 0; i < dolls.length; i++) {
+    if (dolls[i] === 'Hello Kitty' || dolls[i] === 'Barbie doll') {
+      bag.push(dolls[i]);
+    } else {
+      continue;
+    }
+    if (bag.length === 3) break;
+  }
+  return bag;
+}
+
 //http://www.codewars.com/kata/5722b3f0bd5583cf44001000
+
+function giveMeFive(obj) {
+  let arr = [];
+
+  for (const key in obj) {
+    if (key.length === 5) {
+      arr.push(key);
+    }
+    if (obj[key].length === 5) {
+      arr.push(obj[key]);
+    }
+  }
+
+  return arr;
+}
