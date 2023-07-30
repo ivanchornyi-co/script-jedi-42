5c1272e5059c0fd3cd8a1646063e5bf1e7bd0fae
ivanchornyi-co <ivanchornyi.co@gmail.com>
2023-07-30 09:45:48 +0300

    day-06 end

--- day-06.js
+++ day-06.js
@@ -1,5 +1,24 @@
 //https://www.codewars.com/kata/maximum-multiple/train/javascript
 
+function maxMultiple(divisor, bound) {
+  let N = 0;
+  for (let i = 0; i <= bound; i++) {
+    if (i % divisor === 0) {
+      N = i;
+    }
+  }
+  return N;
+}
+
 //https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
 
+function hero(bullets, dragons) {
+  return bullets >= dragons * 2 ? true : false;
+}
+
 //https://www.codewars.com/kata/59441520102eaa25260000bf
+
+function unusualFive() {
+  const str = 'fives';
+  return str.length;
+}
