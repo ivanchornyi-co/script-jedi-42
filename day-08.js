6c3e05a7c64ed3ae1fad946dee174d4e734f5f21
ivanchornyi-co <ivanchornyi.co@gmail.com>
2023-07-31 22:01:35 +0300

    some commit

--- day-08.js
+++ day-08.js
@@ -1,5 +1,48 @@
 //http://www.codewars.com/kata/5722fd3ab7162a3a4500031f
 
+function whatNumberIsIt(n) {
+  if (n === Number.MAX_VALUE) return 'Input number is Number.MAX_VALUE';
+  if (n === Number.MIN_VALUE) return 'Input number is Number.MIN_VALUE';
+  if (isNaN(n)) return 'Input number is Number.NaN';
+  if (n > Number.MAX_VALUE) return 'Input number is Number.POSITIVE_INFINITY';
+  if (n < Number.MIN_VALUE) return 'Input number is Number.NEGATIVE_INFINITY';
+  return `Input number is ${n}`;
+}
+
 //https://www.codewars.com/kata/57238ceaef9008adc7000603
 
+function colorOf(r, g, b) {
+  let red = r.toString(16);
+  if (red.length === 1) {
+    red = '0' + red;
+  }
+
+  let green = g.toString(16);
+  if (green.length === 1) {
+    green = '0' + green;
+  }
+
+  let blue = b.toString(16);
+  if (blue.length === 1) {
+    blue = '0' + blue;
+  }
+
+  return '#' + red + green + blue;
+}
+
 //http://www.codewars.com/kata/57256064856584bc47000611
+
+function howManySmaller(arr, n) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].toFixed(2);
+  }
+
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < n) {
+      count++;
+    }
+  }
+
+  return count;
+}
---
+++ day-09.js
@@ -0,0 +1,5 @@
+//http://www.codewars.com/kata/57274562c8dcebe77e001012
+
+//http://www.codewars.com/kata/57277a31e5e51450a4000010
+
+//http://www.codewars.com/kata/57280481e8118511f7000ffa
